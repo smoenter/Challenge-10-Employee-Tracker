34303ed6749bf55f1853cc32b54b9ea813f5a5e3
smoenter <staceymoenter@gmail.com>
2025-02-03 22:08:52 -0500

    add schema.sql with content

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+--    Create new database --
+    DROP DATABASE IF EXISTS employee_tracker_db;
+    CREATE DATABASE employee_tracker_db;
+
+    -- Use tracker_db --
+   \c employee_tracker_db;
+
+--    Select database in use --
+SELECT current_database(employee_tracker_db);
+
+-- Create talbes --
+CREATE TABLE department (
+    id SERIAL PRIMARY KEY,
+    department_name VARCHAR(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL
+);
+
+CREATE TABLE employee(
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name  VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER
+);
--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,35 @@ import inquirer from 'inquirer';
 // import db connection 
 
 // write the questions 
-// inquirer
-//     .prompt([
-//         {
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the department?',
+        },
+        {
+            type: 'input',
+            name: 'role',
+            message: 'Waht is the name of the role?',
+        },
+        {
+            type: 'number',
+            name: 'salary',
+            message: 'What is the salary of the role?',
+        },
+        {
+           type: 'input',
+           name: 'department',
+           message: 'Which department does the role belong to?',
+        },
+        {
+            type: 'input',
+            name: 'manager',
+            message: 'Who is manager?',
+        },
 
-//         },
-//         {
-
-//         },
-//         {
-
-//         },
-
-//     ])
+    ])
 // give the question to inquirer
 // .then((reponse)=> console.log(colors[response.color](response.text))):
 // put the answers into the db
